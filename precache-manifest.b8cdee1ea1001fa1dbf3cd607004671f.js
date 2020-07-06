231651e3536146e4b7137f730c3ba2d12847426c
GMorse19 <malorse@comcast.net>
2020-07-06 09:12:01 -0400

    Updates

---
+++ precache-manifest.b8cdee1ea1001fa1dbf3cd607004671f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "522a20487ed442246bdf718b4a5e8293",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b147880eb334ea01f005",
+    "url": "/static/css/2.14fbd4c8.chunk.css"
+  },
+  {
+    "revision": "3c91a43479a369ec661d",
+    "url": "/static/css/main.e1a6acde.chunk.css"
+  },
+  {
+    "revision": "b147880eb334ea01f005",
+    "url": "/static/js/2.307e5030.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.307e5030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c91a43479a369ec661d",
+    "url": "/static/js/main.a971c5c4.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "cb6d28ed1412e753d427e95d3eff1627",
+    "url": "/static/media/smiley1.cb6d28ed.png"
+  },
+  {
+    "revision": "b4c1c3382c925bb0914c658e93f8e67e",
+    "url": "/static/media/smiley2.b4c1c338.png"
+  },
+  {
+    "revision": "f3969dabfdb86d91b04e4e8d02ea31bc",
+    "url": "/static/media/smiley3.f3969dab.png"
+  },
+  {
+    "revision": "e2025ea65f370a72c7e50dfa90a9a15b",
+    "url": "/static/media/smiley4.e2025ea6.png"
+  },
+  {
+    "revision": "37ebb46b942faa5526c45901c75e2a89",
+    "url": "/static/media/smiley5.37ebb46b.png"
+  }
+]);
