04571eea2c02c8277bb95598cc9d150d0fa8fcb0
GMorse19 <malorse@comcast.net>
2020-07-23 10:45:34 -0400

    Refactored commented and cleaned up

--- src/App.js
+++ src/App.js
@@ -4,12 +4,47 @@ import './App.scss';
 
 import HappySlider from './HappySlider/HappySlider'
 
-function App() {
-  return (
-    <div className="App">
-      <HappySlider />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+      this.state = {
+        sliderOne: 3,
+        sliderTwo: 3
+      }
+  }
+
+  handleChange = (event) => {
+      this.setState({ [event.target.name]: event.target.value })
+    }
+
+  render() {
+    return (
+      <div className="App">
+        <HappySlider
+          type="range"
+          onChange={this.handleChange}
+          min='1'
+          max='5'
+          value={this.state.sliderOne}
+          name="sliderOne"
+          className='sliderOne'
+          id="sliderOne"
+          step='0.01'
+        />
+        <HappySlider
+          type="range"
+          onChange={this.handleChange}
+          min='1'
+          max='5'
+          value={this.state.sliderTwo}
+          name='sliderTwo'
+          className='sliderTwo'
+          id="sliderTwo"
+          step='0.01'
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
